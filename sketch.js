b03e805b0c10c79eb91d739dee279ffe2cffeed6
Sammii-HK <sammii.h@icloud.com>
2020-01-16 12:16:06 +0000

    visualisations now based on mouse movement

--- sketch.js
+++ sketch.js
@@ -1,9 +1,9 @@
-var scaleX;
-var scaleY;
-var min;
-var max;
-var radius;
-var r, g, b;
+let scaleX
+let scaleY
+let min
+let max
+let radius
+let r, g, b
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -17,19 +17,38 @@ function drawEllipse() {
   ellipse(0, 0, 120, 80)
 }
 
+function mouseMoved() {
+    if (mouseX < (windowWidth/3)){
+        //r = random(255);
+        r = map(mouseY, 0, windowHeight, 0, 255)
+    } else if (mouseX > ((windowWidth/3)-windowWidth)){
+        //b = random(255)
+        b = map(mouseY, 0, windowHeight, 0, 255)
+    } else {
+        //g = random(255)
+        g = map(mouseY, 0, windowHeight, 0, 255)
+    }
+    radius = map(mouseY, 0, windowHeight, 100, 350)
+}
+
+
 function draw() {
   scaleX = map(mouseX, 0, windowWidth, 1.5, 11.5)
   scaleY = map(mouseY, 0, windowHeight, 1.5, 11.5)
+  min = map(mouseX, 0, windowWidth, 0.1, 0.5)
+  max = map(mouseY, 0, windowHeight, 0.8, 1.8)
+  // console.log("scaleX/Y:", scaleX, scaleY)
 
   background(0)
   translate(windowWidth/2, windowHeight/2)
+
   for (let i = 0; i < 360; i += 0.5) {
     push()
     rotate(radians(i))
-    translate(0, 200)
-
+    translate(0, radius)
     rotate(radians(i * 3))
-    scale(map(sin(radians(i * scaleX)), 1, 1, 0.5, 1), map(sin(radians(i * scaleY)), 1, 1, 0.5, 1))
+    scale(map(sin(radians(i*scaleX)), -1, 1, min, max),
+          map(sin(radians(i*scaleY)), -1, 1, min, max))
     drawEllipse()
     pop()
   }
